6eb098b1dc056c5b8549ac6995130410cc8348a3
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-10-17 09:17:33 -0500

    end point video streaming server

---
+++ contentServer/index.js
@@ -0,0 +1,45 @@
+var express = require('express')
+const fs = require('fs')
+
+var createContentServer = function () {
+
+  var app = express()
+
+  app.use(express.static('./public'))
+
+  app.get('/video', function(req, res) {
+    const path = 'assets/videoDemo.mp4'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+  })
+
+  app.listen(8080, '192.168.0.102', function () {
+    console.log('Listening on port 3000!')
+  })
+}
+module.exports = createContentServer
