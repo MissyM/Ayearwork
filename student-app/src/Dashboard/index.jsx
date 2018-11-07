d9a72208a840a9b8d49e2040a6664c7412f6146c
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-11-06 22:37:12 -0500

    working in dashboard (navBar, vertical Menu)

--- student-app/src/Dashboard/index.jsx
+++ student-app/src/Dashboard/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import "./styles.css"
 
 import Main from './Main'
 import Topics from './Topics'
 import Learn from './Learn'
 
 export default function ({ match }) {
-  return <div>
-    Dashboard
-    <Route path={`${match.url}/`} exact component={Main}/>
-    <Route path={`${match.url}/topics`} component={Topics}/>
-    <Route path={`${match.url}/learn/:topicId`} component={Learn}/>
+  return <div className="dashboard">
+    <div className="navBackGray">
+      <div className="navAvobeWhite" />
+      <div className="triangleShadowLow" />
+    </div>
+    <div className="content">
+      <div className="verticalMenu">
+      </div>
+      <div className="dashboardContainer">
+        <Route path={`${match.url}/`} exact component={Main}/>
+        <Route path={`${match.url}/topics`} component={Topics}/>
+        <Route path={`${match.url}/learn/:topicId`} component={Learn}/>
+      </div>
+    </div>
   </div>
 }
