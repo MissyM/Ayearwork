8f2445edeb5e1b0ca70bd4b5391bf9aff546bdba
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-11-09 09:18:45 -0500

    working in Main

--- student-app/src/Dashboard/Main.jsx
+++ student-app/src/Dashboard/Main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import './styles'
+
 export default ({ match }) => (
-  <div>
-    <h1>This is Main!</h1>
-    <Link to="/dashboard/topics">Learn</Link>
+  <div className="containerMain">
+    <h1 className="welcome">¡Hola Ana Isabel!</h1>
+    <p className= "">bla, bla, bla</p>
+    <div className="">
+      <div className="left">
+        <Link to="/dashboard/topics">
+          <div className="aprender">
+            
+          </div>
+        </Link>
+      </div>
+      <div className="right">
+
+      </div>
+    </div>
   </div>
 )
