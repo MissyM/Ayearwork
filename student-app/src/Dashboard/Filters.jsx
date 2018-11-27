219b8f033a6ee8bbfcd99a79ebf5b5715fab35f6
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-11-27 07:33:14 -0500

    Working in the filter

--- student-app/src/Dashboard/Filters.jsx
+++ student-app/src/Dashboard/Filters.jsx
@@ -4,9 +4,20 @@ class Filter extends Component {
   render() {
     return (
       <div className="contentFilters">
-        <h2>Filtro1</h2>
-        <h2>Filtro2</h2>
-        <h2>Filtro3</h2>
+        {/*Titulos de los filtros */}
+
+        <div className="divTitle">Filtro1
+          <div className="buttonDiv"></div>
+        </div>
+        <div className="divTitle">Filtro2
+          <div className="buttonDiv"></div>
+        </div>
+        <div className="divTitle">Filtro3
+          <div className="buttonDiv"></div>
+        </div>
+        {/*Cuerpo del filtros */}
+        <div className="contentBodyFilter">
+        </div>
       </div>
     )
   }
