997c7c502499f362b85729a02dc59435dbedc927
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-11-27 07:44:53 -0500

    Filter container was created

--- student-app/src/Dashboard/Filters.jsx
+++ student-app/src/Dashboard/Filters.jsx
@@ -17,6 +17,27 @@ class Filter extends Component {
         </div>
         {/*Cuerpo del filtros */}
         <div className="contentBodyFilter">
+        {/*Tarjetas del filtro. TODO: Hacer map */}
+          <div className="contentCard">
+            <div>
+              <img className="formatResourceIcon" src="" alt=""/>
+              <div className="card">
+                <div className="titleCard">Titulo del tema</div>
+                <div className="metadata">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Dolore recusandae eos sapiente debitis cumque 
+                  soluta dignissimos ipsa. 
+                  </p>
+                </div>
+                <div className="iconsContent">
+                  <img  className="aprendizajeIcon" src="" alt=""/>
+                  <img  className="entrenarIcon" src="" alt=""/>
+                  <img  className="competirIcon" src="" alt=""/>
+
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
