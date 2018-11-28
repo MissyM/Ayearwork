705bb3455e658e26bbcf48074eee1c815bfee2cb
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-11-28 14:34:17 -0500

    Routing in filters

--- student-app/src/Dashboard/FiltersTitles.jsx
+++ student-app/src/Dashboard/FiltersTitles.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './stylesNavBar.css'
 
@@ -8,28 +9,38 @@ export default class FiltersTitles extends React.Component {
       <div className="contentFilterTitles">
 
         <div clasname= "contentTag">
+          <Link to="/dashboard/todo" className="linksToOfFilters">
           <div className="divTitleTodo">Todo</div>
+          </Link>
           <div className="buttonDivTodo"/>
         </div>
 
         <div clasname= "contentTag">
+          <Link to="/dashboard/filters/pdf" className="linksToOfFilters">
           <div className="divTitlePdf">Pdf</div>
-          <div className="buttonDivPdf"></div>
+          <div className="buttonDivPdf"/>
+          </Link>
         </div>
         
         <div clasname= "contentTag">
+          <Link to="/dashboard/filters/video" className="linksToOfFilters">
           <div className="divTitleVideo">Video</div>
-          <div className="buttonDivVideo"></div>
+          <div className="buttonDivVideo"/>
+          </Link>
         </div>
 
         <div clasname= "contentTag">
+          <Link to="/dashboard/filters/ppt" className="linksToOfFilters">
           <div className="divTitlePpt">Ppt</div>
-          <div className="buttonDivPpt"></div>
+          <div className="buttonDivPpt"/>
+          </Link>
         </div>
 
         <div clasname= "contentTag">
+          <Link to="/dashboard/filters/todo" className="linksToOfFilters">
           <div className="divTitleTema">Tema</div>
-          <div className="buttonDivTema"></div>
+          <div className="buttonDivTema"/>
+          </Link>
         </div>
         
       </div>
