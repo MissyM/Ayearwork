d084d3d14e679d72af7f20d516d965b5a18df21c
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-12-04 15:48:04 -0500

    Implementing styled components library

--- student-app/src/Dashboard/Tema.jsx
+++ student-app/src/Dashboard/Tema.jsx
@@ -1,72 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const tema= {
-  display: 'flex',
-  width: '80%',
-  height: '100px',
-  marginTop: '30px',
-}
-const cardTema= {
-  width:' 80%',
-  color: '#B1B1B2',
-}
-const titleCardTema= {
-  padding: '15px 0px 0px 15px',
-  fontWeight: 'bolder',
-}
-const metadataTema= {
-  padding: '0px 15px',
-  fontSize: '14px',
-}
-const iconsContent= {
-  position: 'absolute',
-  right: '200px',
-  height: '30px',
-}
-const aprendizajeIcon= {
-  height: '30px',
-  width: '30px',
-  margin: '0px 10px',
-  border: '2px solid  rgb(209, 214, 209)',
-  borderRadius: '50px',
-  background:  'rgb(209, 214, 209)',
-}
-const entrenarIcon= {
-  height: '30px',
-  background: 'rgb(209, 214, 209)',
-  border: '2px solid  rgb(209, 214, 209)',
-  borderRadius: '50px',
-  width: '30px',
-  margin: '0px 10px',
-}
-const competirIcon= {
-  width: '30px',
-  height: '30px',
-  background:  'rgb(209, 214, 209)',
-  border: '2px solid  rgb(209, 214, 209)',
-  borderRadius: '50px',
-  margin: '0px 10px',
-}
+const Card= styled.div `
+  width: 50%;
+  color: #B1B1B2;
+`
+const Title= styled.div `
+  padding: 15px 0px 0px 15px;
+  font-weight: bolder;
+`
+const Ul= styled.ul `
+  padding: 0px 15px;
+  font-size: 14px;
+`
+const Content= styled.div`
+  position: absolute;
+  right: 200px;
+  height: 30px;
+  `
+const AprendizajeIcon= styled.img` 
+  height: 30px;
+  width: 30px;
+  margin: 0px 10px;
+  border: 2px solid  rgb(209, 214, 209);
+  border-radius: 50px;
+  background:  rgb(209, 214, 209);
+  `
+const EntrenarIcon= styled.img`
+  height: 30px;
+  background: rgb(209, 214, 209);
+  border: 2px solid  rgb(209, 214, 209);
+  border-radius: 50px;
+  width: 30px;
+  margin: 0px 10px;
+`
+const CompetirIcon= styled.img`
+  width: 30px; 
+  height: 30px;
+  background:  rgb(209, 214, 209);
+  border: 2px solid  rgb(209, 214, 209);
+  border-radius: 50px;
+  margin: 0px 10px;
+`
 
 export default class Tema extends React.Component {
   render() {
     return (
-      <div style={tema}>
-        <div style={cardTema}>
-          <div style={titleCardTema}>1.Números Naturales</div>
-          <div style={metadataTema}>
-            <ul>
-              <li>1.1 Sistema de Numeración</li>
-              <li>1.2 Operaciones en el conjunto de los números naturales</li>
-            </ul>
-          </div>
-          <div style={iconsContent}>
-            <img  style={aprendizajeIcon} src={require("./assetsDashboard/ic_aprendizaje-01.svg")} alt="Aprendizaje"/>
-            <img  style={entrenarIcon} src={require("./assetsDashboard/ic_entrenamiento-01.svg")} alt="Entrenamiento"/>
-            <img  style={competirIcon} src={require("./assetsDashboard/ic_competencia.svg")} alt="Competencia"/>
-          </div>
-        </div>
-      </div>
+      <Card>
+        <Title >1.Números Naturales</Title>
+          <Ul>
+            <li>1.1 Sistema de Numeración</li>
+            <li>1.2 Operaciones en el conjunto de los números naturales</li>
+          </Ul>
+        <Content>
+          <img  style={aprendizajeIcon} src={require("./assetsDashboard/ic_aprendizaje-01.svg")} alt="Aprendizaje"/>
+          <img  style={entrenarIcon} src={require("./assetsDashboard/ic_entrenamiento-01.svg")} alt="Entrenamiento"/>
+          <img  style={competirIcon} src={require("./assetsDashboard/ic_competencia.svg")} alt="Competencia"/>
+        </Content>
+      </Card>
     ) 
   }
 }
