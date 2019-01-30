248df298a2a5c63224824a9eb1b5cb7c4c1e0a66
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2019-01-30 18:29:30 -0500

    Adding some coments

--- electron/buildStudentApp.js
+++ electron/buildStudentApp.js
@@ -1,11 +1,11 @@
 // build script
-
+// Se importa la librería shell 
 const shell = require('shelljs')
-
+//Sale de la carpeta de electron
 shell.cd('..')
-
+//Entra en la carpeta student-app
 shell.cd('student-app')
-
+//el metodo .exec ejecuta un comando npm y cp copia el contenido de build a student-app-compiled
 shell.exec('npm run build', () => {
   shell.cp('-Rf', 'build/*', '../electron/student-app-compiled')
 })
