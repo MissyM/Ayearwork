47f321845bebc06c93ce25b2f62aee8d44361b45
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2019-02-03 13:10:26 -0500

    Adding Topics to assets folder

--- student-app/src/model/index.js
+++ student-app/src/model/index.js
@@ -5,204 +5,225 @@ export const model = {
       id: 'numerosNaturales',
       title: '1. Numeros Naturales',
       subtopics: [
+        //Subtema 1.1
         {
           id: 'sistemadeNumeracion',
-          title: 'Sistemas de Numeración',
-          resources: [],
+          title: '1.1 Sistemas de Numeración',
+          resources: [
+            {
+              id: '1.1.1 SISTEMA DE NUMERACION.pdf',
+              type: 'pdf',
+              title: '1.1.1 Sistema de Numeración - 1',
+              description: 'Descripción del tema'
+            },
+            {
+              id: '1.1.2 SISTEMA DE NUMERACION.mp4',
+              type: 'video',
+              title: '1.1.2 Sistema de Numeración - 2',
+              description: 'Descripción del tema'
+            },
+          ],
         },
+        //Subtema 1.2
         {
           id: 'operacionesenelConjuntodelosNumerosNaturales',
-          title: 'Operaciones en el conjunto de números naturales',
-          resources: [],
+          title: '1.2 Operaciones en el Conjunto de Números Naturales',
+          resources: [
+            {
+              id: '1.2.1 OPERACIONES EN EL CONJUNTO DE LOS NUMEROS NATURALES.mp4',
+              type: 'video',
+              title: '1.2.1 Operaciones en el Conjunto de Números Naturales - 1',
+              description: 'Descripción del tema'
+            },
+            {
+              id: '1.2.2 OPERACIONES EN EL CONJUNTO DE LOS NUMEROS NATURALES.pdf',
+              type: 'pdf',
+              title: '1.2.2 Operaciones en el Conjunto de Números Naturales - 2',
+              description: 'Descripción del tema'
+            },
+          ],
         },
       ],
     },
     {
+      //Tema
       id: 'variacionyEcuaciones',
       title: '2. Variacion y Ecuaciones',
       subtopics: [
+        //Subtema 2.1
         {
           id: 'nocinesdeCambio',
-          title: 'Nociones de cambio',
-          resources: [],
+          title: '2.1 Nociones de cambio',
+          resources: [
+            {
+              id: '2.1.1 NOCIONES DE CAMBIO.mp4',
+              type: 'video',
+              title: '2.1.1 Nociones de Cambio - 1',
+              description: 'Descripción del tema'
+            },
+            {
+              id: '2.1.2 NOCIONES DE CAMBIO.pdf',
+              type: 'pdf',
+              title: '2.1.2 Nociones de Cambio- 2',
+              description: 'Descripción del tema'
+            },
+          ],
         },
+        //Subtema 2.2
         {
           id: 'ecuaciones',
-          title: 'Ecuaciones',
-          resources: [],
+          title: '2.2 Ecuaciones',
+          resources: [
+            {
+              id: '2.2.1 ECUACIONES.pdf',
+              type: 'pdf',
+              title: '2.2.1 Ecuaciones - 1',
+              description: 'Descripción del tema'
+            },
+            {
+              id: '2.2.2 ECUACIONES.mp4',
+              type: 'video',
+              title: '2.2.2 Ecuaciones- 2',
+              description: 'Descripción del tema'
+            },
+          ],
         },
       ],
     },
     {
+      //Tema
       id: 'teoriadelosNumeros',
       title: '3. Teoria de los Números',
       subtopics: [
+        //Subtema 3.1
         {
           id: 'multiplosyDivisores',
-          title: 'Múltiplos y divisores',
-          resources: [],
+          title: '3.1 Múltiplos y divisores',
+          resources: [
+            {
+              id: '3.1.1 MULTIPLOS Y DIVISORES.pdf',
+              type: 'pdf',
+              title: '3.1.1 Multiplos y Divisores -1',
+              description: 'Descripción del documento ' 
+            },
+            {
+              id: '3.1.2 MULTIPLOS Y DIVISORES.mp4',
+              type: 'video',
+              title: '3.1.2 Multiplos y Divisores -2',
+              description: 'Descripción del documento ' 
+            },
+          ],
         },
+        //Subtema 3.2
         {
           id: 'numerosPrimosyCompuestos',
-          title: 'Números primos y compuestos',
-          resources: [],
+          title: '3.2 Números Primos y Compuestos',
+          resources: [
+            {
+              id: '3.2.1 NUMEROS PRIMOS Y COMPUESTOS.pdf',
+              type: 'pdf',
+              title: '3.2.1 Número Prímos y Compuestos -1',
+              description: 'Descripción del documento'
+            },
+            {
+              id: '3.2.2 NUMEROS PRIMOS Y COMPUESTOS.mp4',
+              type: 'video',
+              title: '3.2.2 Número Prímos y Compuestos -2',
+              description: 'Descripción del documento'
+            },
+          ],
         },
+        //Subtema 3.3
         {
           id: 'mcd',
-          title: 'MCD',
-          resources: [],
+          title: '3.3 MCD',
+          resources: [
+            {
+              id: '3.3.1 MCD.pdf',
+              type: 'pdf',
+              title: '3.3.1 MCD -1.',
+              description: 'Descripción del documento'
+            },
+            {
+              id: '3.3.2 MCD.mp4',
+              type: 'video',
+              title: '3.3.2 MCD -2.',
+              description: 'Descripción del documento'
+            },
+          ],
         },
       ],
     },
     {
-      id: 'fraccionariosyDecimales',
-      title: '4. Fraccionarios y Decimales',
+      //Tema
+      id: 'fracciones',
+      title: '4. Fracciones',
+      //Subtemas
       subtopics: [
+        //Subtema 4.1
         {
           id: 'fracciones',
           title: '4.1 Fracciones',
-          resources: [],
+          resources: [
+            {
+              id: '4.1.1 FRACCIONES.pdf',
+              type: 'pdf',
+              title: '4.1.1 Fracciones -2',
+              description: 'Dos fracciones son homogéneas cuando tienen el mismo denominador. Cuando se tienen fracciones homogéneas, se debe operar (dependiendo de si se trata de una suma o una resta) únicamente los numeradores y mantener el denominador.  ' 
+            },
+            {
+              id: '4.1.2 FRACCIONES.mp4',
+              type: 'video',
+              title: '4.1.2 Fracciones -1',
+              description: 'Dos fracciones son homogéneas cuando tienen el mismo denominador. Cuando se tienen fracciones homogéneas, se debe operar (dependiendo de si se trata de una suma o una resta) únicamente los numeradores y mantener el denominador.  ' 
+            },
+          ],
         },
+        //Subtema 4.2
         {
           id: 'operacionesConFracciones',
-          title: '4.2 Operaciones con fracciones',
+          title: '4.2 Operaciones con Fracciones',
           resources: [
-            // Homo
             { 
-              id: 'SUMA Y RESTA DE FRACCIONES HOMOGENEAS-MARCELA.mp4',
-              type: 'video',
-              title: 'Suma y resta de fracciones homogéneas - 1',
-              description: 'lorem asdasdasd adsdsdf sdfdf' 
+              id: '4.2.1 SUMA Y RESTA FRACCIONES HOMOGENEAS.pdf',
+              type: 'pdf',
+              title: '4.2.1 Suma y Resta de Fracciones Homogéneas - 1',
+              description: 'Dos fracciones son homogéneas cuando tienen el mismo denominador. Cuando se tienen fracciones homogéneas, se debe operar (dependiendo de si se trata de una suma o una resta) únicamente los numeradores y mantener el denominador.  ' 
             },
-            
             {
-             id: '1_1 SUMA Y RESTA FRACCIONES HOMOGENEAS.pdf',
-             type: 'pdf',
-             title: 'Suma y resta de fracciones homogéneas - 2',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: 'SUMA FRACCIONES HOMOGENEAS-YOUTUBE.mp4',
+             id: '4.2.2 SUMA Y RESTA FRACCIONES HOMOGENEAS-MARCELA.mp4',
              type: 'video',
-             title: 'Suma de fracciones homogéneas',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
+             title: '4.2.2 Suma y Resta de Fracciones Homogéneas  - 2',
+             description: 'Dos fracciones son homogéneas cuando tienen el mismo denominador. Cuando se tienen fracciones homogéneas, se debe operar (dependiendo de si se trata de una suma o una resta) únicamente los numeradores y mantener el denominador.  ' 
             },
-            
+            /*----------------- */
             {
-             id: 'RESTA DE FRACCIONES HOMOGENEAS-YOUTUBE.mp4',
+             id: '4.2.3 SUMA Y RESTA DE FRACCIONES HETEROGENEAS.mp4',
              type: 'video',
-             title: 'Resta de fracciones homogéneas',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
+             title: '4.2.3 Suma y resta de fracciones heterogéneas -1',
+             description: 'Dos fracciones son heterogéneas cuando tienen diferente denominador. Para sumar o restar fracciones homogéneas, es necesario conocer el proceso para determinar el mínimo común múltiplo (m.c.m) de dos o más números, debido a que se requiere calcular el m.c.m de los denominadores.' 
+            },
+            {
+             id: '4.2.4 SUMA Y RESTA DE FRACCIONES HETEROGENEAS.pdf',
+             type: 'pdf',
+             title: '4.2.4 Suma y Resta de Fracciones Heterogeneas -2',
+             description: 'Dos fracciones son heterogéneas cuando tienen diferente denominador. Para sumar o restar fracciones homogéneas, es necesario conocer el proceso para determinar el mínimo común múltiplo (m.c.m) de dos o más números, debido a que se requiere calcular el m.c.m de los denominadores.' 
+            },
+             /*----------------- */
+            {
+             id: '4.2.5 MULTIPLICACION Y DIVISION DE FRACCIONES.pdf',
+             type: 'pdf',
+             title: '4.2.5 Multiplicación y División de Fracciones -1',
+             description: 'Para multiplicar dos fracciones, bien sea homogéneas o heterogéneas el producto de los numeradores se convierte en el numerador de la fracción resultante e igualmente el producto de los denominadores, se convierte en el denominador de la fracción resultante. La fracción resultante se simplifica de ser necesario.' 
             },
             
-            // Hetero
-            { 
-             id: 'SUMA FRACCIONES HETEROGENEAS-YOUTUBE.mp4',
+            {
+             id: '4.2.6 MULTIPLICACIÓN Y DIVISION DE FRACCIONES.mp4',
              type: 'video',
-             title: 'Suma y resta de fracciones heterogéneas - 1',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
+             title: '4.2.6 Multiplicación y División de Fracciones -2',
+             description: 'Para dividir dos fracciones, bien sea homogéneas o heterogéneas, se debe multiplicar el numerador de la primera fracción por el denominador de la segunda e igualmente el denominador de la primera fracción por el numerador de la segunda, el primer resultado se convierte en el numerador de la fracción resultante y el segundo resultado se convierte en su denominador. La fracción resultante se simplifica de ser necesario.' 
             },
-            
-            {
-             id: 'SUMA FRACCIONES HETEROGENEAS-ESTUDIANTE.mp4',
-             type: 'video',
-             title: 'Suma y resta de fracciones heterogéneas - 2',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '1_2 SUMA Y RESTA FRACCIONES HETEROGENEAS.pdf',
-             type: 'pdf',
-             title: 'Suma y resta de fracciones heterogéneas',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '1_3 EJERCICIOS SOBRE SUMA Y RESTA DE FRACCIONES DE IGUAL DENOMINADOR.pdf',
-             type: 'pdf',
-             title: 'Ejercicios sobre suma y resta de fracciones de igual denominador',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            // mult
-            {
-             id: 'Multiplicacion y Division de fracciones.mp4',
-             type: 'video',
-             title: 'Multiplicación y división de fracciones',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: 'MULTIPLICACION DE FRACCIONES YOUTUBE.mp4',
-             type: 'video',
-             title: 'Multiplicación de fracciones',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: 'DIVISION DE FRACCIONES.mp4',
-             type: 'video',
-             title: 'División de fracciones',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '2_1 MULTIPLICACION DE FRACCIONES.pdf',
-             type: 'pdf',
-             title: 'Multiplicación de fracciones',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '3_1 DIVISION DE FRACCIONES.pdf',
-             type: 'pdf',
-             title: 'División de fracciones - 1',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '3_2 DIVISION DE FRACCIONES.pdf',
-             type: 'pdf',
-             title: 'División de fracciones - 2',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '3_3 EJERCICIOS DE MULTIPLICACION Y DIVISION DE FRACCIONES.pdf',
-             type: 'pdf',
-             title: 'Ejercicios de multiplicacion y division de fracciones',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '4_1 SUMA, RESTA, MULTIPLICACION Y DIVISION DE FRACCIONARIOS.pdf',
-             type: 'pdf',
-             title: 'Suma, resta, multiplicacion y division de fraccionarios - 1',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '4_2 SUMA, RESTA, MULTIPLICACION Y DIVISION DE FRACCIONARIOS.pdf',
-             type: 'pdf',
-             title: 'Suma, resta, multiplicacion y division de fraccionarios - 2',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '4_3 SUMA, RESTA, MULTIPLICACION Y DIVISION DE FRACCIONARIOS.pdf',
-             type: 'pdf',
-             title: 'Suma, resta, multiplicacion y division de fraccionarios - 3',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
-            {
-             id: '4_4 SUMA, RESTA, MULTIPLICACION Y DIVISION DE FRACCIONARIOS.pdf',
-             type: 'pdf',
-             title: 'Suma, resta, multiplicacion y division de fraccionarios - 4',
-             description: 'lorem asdasdasd adsdsdf sdfdf' 
-            },
-            
+             /*----------------- */
           ],
         },
       ],
