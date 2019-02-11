ec6ee4c077a4262f5692f4a0b054df26640a2b1d
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2019-02-11 16:33:42 -0500

    Optimizing the Browser component

--- student-app/src/Dashboard/Browser/index.jsx
+++ student-app/src/Dashboard/Browser/index.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import TopicSelect from '../components/TopicSelect'
+import styled from 'styled-components'
 
-import './styles.css'
+const ContentBrowserComponent = styled.div`
+  position:relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  background: #ffffff;
+  /* z-index: -1; */
+`
+const LoboYupay = styled.img.attrs ({
+  src: require('../assetsDashboard/lobo+yupay.svg'),
+  alt: "Logo",
+})`
+  height: 300px;
+  width: 300px;
+  margin: 0px  200px;
+  margin-top: -200px;
+`
+const Select = styled.div`
+  width: 450px;
+`
 
 export default () => (
-  <div className="contentBrowserComponent">
-    <img className="loboYupay" src={require('../assetsDashboard/lobo+yupay.svg')} alt="Logo"/>
-    <div className='select'>
+  <ContentBrowserComponent>
+    <LoboYupay/>
+    <Select>
       <TopicSelect />
-    </div>
-  </div>
+    </Select>
+  </ContentBrowserComponent>
 )
