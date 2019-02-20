a51ed2fd7aaf1812053569cfe575961925c3c42e
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2019-02-20 06:44:56 -0500

    Deletoing comments

--- student-app/src/services/api.js
+++ student-app/src/services/api.js
@@ -42,36 +42,3 @@ export const getRelatedResources = id => {
   return resources
 }
 
-/**
-export const getContent = searchString => {
-  const params = new URLSearchParams(searchString)
-  const type = params.get('type') || 'all'
-  const option = params.get('option')
-  const id = params.get('id')
-  if (option === 'topic' && type === 'all') {
-    const topic = model.topics.find(topic => topic.id === id)
-    const result = [{ ...topic, type: 'topic' }]
-    for (const subtopic of topic.subtopics) {
-      result.push(...subtopic.resources)
-    }
-    return result
-  }
-  if (option === 'subtopic' && type === 'all') {
-    const subtopic = subtopics.find(subtopic => subtopic.id === id)
-    return subtopic.resources
-  }
-  if (type === 'all') {
-    return [
-      ...model.topics.map(topic => ({ ...topic, type: 'topic' })),
-      ...resources
-    ] 
-  }
-  if (type === 'topic') {
-    return model.topics.map(topic => ({ ...topic, type: 'topic' }))
-  }
-  if (type === 'pdf' || type === 'video') {
-    return resources.filter(resource => resource.type === type)
-  }
-} 
-
- */
