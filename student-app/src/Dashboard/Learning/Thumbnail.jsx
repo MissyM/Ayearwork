2f8b9f1a0c27d704fa4384870b296d1bd79ef46a
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2019-02-21 13:45:51 -0500

    Adding thumnnail component

---
+++ student-app/src/Dashboard/Learning/Thumbnail.jsx
@@ -0,0 +1,10 @@
+<ul>
+<li class="video">
+  <a  href="https://www.youtube.com/embed/C4jk51rgfts?autoplay=1&controls=0&showinfo=0&color=white&rel=0" class="featured-video">
+    <figure style="background-image: url(https://img.youtube.com/vi/C4jk51rgfts/hqdefault.jpg);">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
+      <figcaption>Will It Go 10s? Mazdarati Is Back!</figcaption>
+    </figure>
+  </a>
+</li>
+</ul>
