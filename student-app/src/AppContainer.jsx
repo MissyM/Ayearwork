8a08f8cf88d4bc4e77b79278517fb24762dfcbba
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2019-03-12 14:41:34 -0500

    stable version pre v2

--- student-app/src/AppContainer.jsx
+++ student-app/src/AppContainer.jsx
@@ -2,7 +2,24 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { logNavego } from './services/log'
+
 class AppContainer extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   const session = {
+  //     userName: '',
+  //   grade: '',
+  //   gender: '',
+  //   password:'',
+  //   age: '',
+  //   avatar: '',
+  //   LearningStyle:'ESTILO ACTIVO',
+  //   Intelligence:'cinetico-corporal'
+  //   }
+  //   this.setSession = (state) => {
+
+  //   }
+  // }
   componentDidMount() {
     logNavego(this.props.history.location.pathname + this.props.history.location.search )
     this.unlisten = this.props.history.listen(location => {
@@ -14,7 +31,10 @@ class AppContainer extends Component {
   }
   render() {
     return (
+      // <SessionCtx.Provider value={[session, setSession]}>
       <>{this.props.children}</>
+      // </SessionCtx.Provider>
+
     )
   }
 }
