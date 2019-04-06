a0e36fe55578a300a642985ba29f43a45255c814
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2019-04-06 11:47:46 -0500

    Adding rewireReactHotLoader

--- student-app/config-overrides.js
+++ student-app/config-overrides.js
@@ -1,6 +1,20 @@
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const rewireStyledComponents = require('react-app-rewire-styled-components')
+const { override } = require('customize-cra')
 
-module.exports = function override(config, env) {
-  config = rewireReactHotLoader(config, env);
-  return config;
+const reactHotLoader = env => config => {
+  config = rewireReactHotLoader(config, env)
+  return config
 }
+
+const styledComponents = obj => config => {
+  config = rewireStyledComponents(config, process.env.NODE_ENV, obj)
+  return config
+}
+
+module.exports = override(
+  reactHotLoader(),
+  styledComponents({
+    displayName: process.env.NODE_ENV !== 'production'
+  })
+)
